cebfbb07c9acc94ec97d52c4729444e648d02991
rj7shakya <shakyarajad1@gmail.com>
2020-03-30 00:24:56 +0545

    move xero

--- med/src/MoveZero.java
+++ med/src/MoveZero.java
@@ -13,7 +13,7 @@ public class MoveZero {
     }
 
     public static void main(String[] args) {
-        int arr[] = {1,9,6,0,3,4,0};
+        int arr[] = {1,2,3,0,0,4,6,1};
         push(arr);
         for (int i=0;i<arr.length;i++){
             System.out.println(arr[i]);
