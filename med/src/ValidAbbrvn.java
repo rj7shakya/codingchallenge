59bcb534dfde12022ce8aa2082f5096281f707c6
rj7shakya <shakyarajad1@gmail.com>
2020-03-29 23:27:59 +0545

    movezero

--- med/src/ValidAbbrvn.java
+++ med/src/ValidAbbrvn.java
@@ -1,2 +1,23 @@
 public class ValidAbbrvn {
+
+    static boolean valid(String s, String abbr){
+        int ptr1 = 0;
+        int ptr2 = 0;
+        while (ptr1<s.length() && ptr2<abbr.length()){
+            if(s.charAt(ptr1) == abbr.charAt(ptr2)){
+                ptr1++;
+                ptr2++;
+                continue;
+            }
+            if (abbr.charAt(ptr2)>='0' && abbr.charAt(ptr2)<='9'){
+                return false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "internationalization", abbr = "i12iz4n";
+
+
+    }
 }
