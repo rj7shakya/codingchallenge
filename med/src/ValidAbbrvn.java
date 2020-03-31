705a0c9e80570b784b6920efc64984d6b289d4a6
rj7shakya <shakyarajad1@gmail.com>
2020-03-31 22:18:47 +0545

    valid abbr

--- med/src/ValidAbbrvn.java
+++ med/src/ValidAbbrvn.java
@@ -9,23 +9,29 @@ public class ValidAbbrvn {
                 ptr2++;
                 continue;
             }
-//            if (abbr.charAt(ptr2)<='0' || abbr.charAt(ptr2)>='9'){
-//                return false;
-//            }
+            if (abbr.charAt(ptr2)<='0' || abbr.charAt(ptr2)>='9'){
+                return false;
+            }
 
             int start = ptr2;
-            while (ptr2 < abbr.length() && ptr2 < abbr.length() && abbr.charAt(ptr2)>='0'&& abbr.charAt(ptr2)<='9') {
+            while (ptr2 < abbr.length()
+                    && abbr.charAt(ptr2)>='0'&& abbr.charAt(ptr2)<='9') {
                 ptr2++;
             }
             int num = Integer.valueOf(abbr.substring(start,ptr2));
-            ptr2 +=num;
+            System.out.println(num);
+            ptr1 +=num;
+//            System.out.println(ptr1+' '+ptr2);
 
         }
+        System.out.println(ptr1);
+        System.out.println(ptr2);
         return ptr1==s.length() && ptr2==abbr.length();
     }
 
+
     public static void main(String[] args) {
-        String s = "aple", abbr = "a2e";
+        String s = "apple", abbr = "a2e";
         System.out.println(valid(s,abbr));
 
     }
